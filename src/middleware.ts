67aa99a3a13ea377602539cd607c345455d9f26a
Yadnesh <yadnesh.narawade22@gmail.com>
2025-09-29 11:47:59 +0530

    fix middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
-export default withAuth(async function middleware() {}, {
+export default withAuth(function middleware() {}, {
   // Middleware still runs on all routes.
   publicPaths: ["/"],
 });
