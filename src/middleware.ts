78918fddcc02e8ad3ac38fa74730436602cf2011
Yadnesh <yadnesh.narawade22@gmail.com>
2025-09-29 11:52:40 +0530

    fix middleware error

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
-export default withAuth({
+export default withAuth(async function middleware() {}, {
+  // Middleware still runs on all routes, but doesn't protect the blog route
   publicPaths: ["/"],
 });
 
