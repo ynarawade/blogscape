dd1e0a9c6d0e49a7baaabf80d7d59f5b17870c80
Yadnesh <yadnesh.narawade22@gmail.com>
2025-09-29 11:37:16 +0530

    updated navbar name

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -20,7 +20,7 @@ async function Navbar() {
       <div className="flex items-center space-x-8">
         <Link href="/">
           <h1 className="text-2xl font-bold text-gray-900">
-            Blog<span className="text-blue-600">Yadnesh</span>
+            Blog<span className="text-blue-600">scape</span>
           </h1>
         </Link>
 
